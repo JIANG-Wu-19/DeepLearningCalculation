7981d7751ef7b4cd42a1d41b3b3edd797dda2e05
JIANG <870027163@qq.com>
2024-04-09 00:17:56 +0800

    feat: nn

--- pytorch/nn/LeNet.py
+++ pytorch/nn/LeNet.py
@@ -10,31 +10,31 @@ class LeNet(nn.Module):
         self.maxPool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.flatten = nn.Flatten()
         self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc1 = nn.Linear(16 * 6 * 6, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, X):
         X = self.conv1(X)
-        X = self.sigmoid(X)
-        # X = self.relu(X)
+        # X = self.sigmoid(X)
+        X = self.relu(X)
         X = self.avgPool(X)
 
         X = self.conv2(X)
-        X = self.sigmoid(X)
-        # X = self.relu(X)
+        # X = self.sigmoid(X)
+        X = self.relu(X)
         X = self.avgPool(X)
 
         X = self.flatten(X)
 
         X = self.fc1(X)
-        X = self.sigmoid(X)
-        # X = self.relu(X)
+        # X = self.sigmoid(X)
+        X = self.relu(X)
 
         X = self.fc2(X)
-        X = self.sigmoid(X)
-        # X = self.relu(X)
+        # X = self.sigmoid(X)
+        X = self.relu(X)
 
         pred = self.fc3(X)
         return pred
